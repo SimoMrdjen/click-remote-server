f98d5807f76a93b523c7c07f873294d5c2e3075d
Simo Mrdjen <dr.sizni@gmail.com>
2024-09-12 12:06:52 +0200

    22

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-const PORT = process.env.PORT || 3000; // Use Heroku's port or fallback to 3000 for local development
-const io = require('socket.io')(PORT);
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
-/*for local deployment
-const io = require('socket.io')(3000);*/
+// Create an Express app
+const app = express();
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  
+// Create an HTTP server
+const server = http.createServer(app);
+
+// Attach socket.io to the HTTP server
+const io = socketIo(server);
+
+// Use Heroku's dynamic port, or default to 3000 for local development
+const PORT = process.env.PORT || 3000;
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+
   // Listen for 'click' events from mobile app
-  socket.on('click', () => {
-    console.log('Received click event');
-    socket.broadcast.emit('performClick'); // Broadcast event to connected clients (PC)
+  socket.on("click", () => {
+    console.log("Received click event");
+    socket.broadcast.emit("performClick"); // Broadcast event to connected clients (PC)
   });
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 });
